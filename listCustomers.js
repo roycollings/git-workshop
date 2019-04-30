8c10ba0d1832513d00826de086521546eea50c61
Roy Collings <roy.collings@gmail.com>
2019-04-30 11:18:23 +0100

    Added address to customers

--- listCustomers.js
+++ listCustomers.js
@@ -1,9 +1,10 @@
 const customers = require('./customers.json');
 
 customers.forEach(customer => {
-    const { name, dob } = customer;
+    const { name, dob, address } = customer;
 
     console.log('');
     console.log('Name:', name);
     console.log('Date of birth:', dob);
+    console.log('Address:', address);
 })
